bb5111dadc4212c44bd8e1743618e4e028e2f4e3
Qbicz <kubicz@student.agh.edu.pl>
2016-07-12 00:24:34 +0200

    user-password simple GUI

---
+++ loginKivy.py
@@ -0,0 +1,34 @@
+# Simple User/Password page from Kivy tut
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+
+# Root widget
+class LoginScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        # implement the functionality of the original class being overloaded
+        super(LoginScreen, self).__init__(**kwargs)
+        
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
+
+class MyApp(App):
+
+    def build(self):
+        return LoginScreen()
+
+
+if __name__ == "__main__":
+    MyApp().run()
+
+
