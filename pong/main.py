08eaace4888ff8b3a0eaa50c92343c7853da05b9
Qbicz <kubicz@student.agh.edu.pl>
2016-07-18 01:47:07 +0200

    kivy tutorial pong game

---
+++ pong/main.py
@@ -0,0 +1,53 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, \
+        ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
+
+
+
+class PongGame(Widget):
+    ball = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball_center = self.center
+        self.ball.velocity = Vector(4,0).rotate(randint(0, 360))
+
+    def update(self, dt):
+        self.ball.move()
+
+        # bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity *= -1
+
+        # bounce off left/right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
+
+
+class PongBall(Widget):
+    # velocity of the ball
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    # reference list property
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    # move the ball one step - function will be called in equal intervals
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
+
+class PongApp(App):
+    def build(self):
+        game = PongGame()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
+
+
+if __name__ == '__main__':
+    PongApp().run()
+
