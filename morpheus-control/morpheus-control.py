f179da16485a8b89ae48f92ba5946d71946383cc
Qbicz <kubicz@student.agh.edu.pl>
2016-07-29 03:53:27 +0200

    grid layout for morpheus control

--- morpheus-control/morpheus-control.py
+++ morpheus-control/morpheus-control.py
@@ -2,7 +2,8 @@
 
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
 
 class MorpheusApp(App):
     connection = None
@@ -13,8 +14,15 @@ class MorpheusApp(App):
 
     def setup_gui(self):
         self.label = Label(text='Morpheus 1 control panel')
-        self.layout = BoxLayout(orientation = 'vertical')
+        self.controlModeBtn = Button(text='Human/Autonomous')
+        self.empty13 = Label(text='')
+        self.empty21 = Label(text='')
+
+        self.layout = GridLayout(cols = 3)
+        self.layout.add_widget(self.controlModeBtn)
         self.layout.add_widget(self.label)
+        self.layout.add_widget(self.empty13)
+        self.layout.add_widget(self.empty21)
         return self.layout
 
 if __name__ == '__main__':
